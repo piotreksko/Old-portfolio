5b9e81bc3fdfe55783a7d18a14cf0b42ba485e51
Piotr Skorupa <35628226+piotreksko@users.noreply.github.com>
2018-02-05 12:19:11 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+  $(window).on("scroll", function() {
+    if ($(window).scrollTop() >= 250) {
+      $(".navbar").addClass("compressed");
+    } else {
+      $(".navbar").removeClass("compressed");
+    }
+  });
+  $(document).ready(function(){
+    var animTime = 300,
+        clickPolice = false;
+
+    $(document).on('touchstart click', '.acc-btn', function(){
+      if(!clickPolice){
+         clickPolice = true;
+
+        var currIndex = $(this).index('.acc-btn'),
+            targetHeight = $('.acc-content-inner').eq(currIndex).outerHeight();
+
+        $('.acc-btn h1').removeClass('selected');
+        $(this).find('h1').addClass('selected');
+
+        $('.acc-content').stop().animate({ height: 0 }, animTime);
+        $('.acc-content').eq(currIndex).stop().animate({ height: targetHeight }, animTime);
+
+        setTimeout(function(){ clickPolice = false; }, animTime);
+      }
+
+    });
+  });
+
+
+  window.sr = ScrollReveal();
+  if(window.innerWidth > 568){
+  sr.reveal('.sr-icons', {
+    duration: 1100,
+    scale: 0.3,
+    distance: '100px'
+  }, 400);
+  sr.reveal('.sr-icons2', {
+    duration: 1100,
+    scale: 0.3,
+    distance: '-100px'
+  }, 400);
+  sr.reveal('.sr-sub', {
+    duration: 1000,
+    scale: 0.9,
+    distance: '-100px'
+  }, 100);}
+});
